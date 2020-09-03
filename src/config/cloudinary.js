5ae931a033dd808b66bfa274c6298872a4827ef6
Elijah Bobzom <ebobzom@gmail.com>
2020-09-03 09:47:44 +0100

    view connection object 2

--- src/config/cloudinary.js
+++ src/config/cloudinary.js
@@ -4,9 +4,9 @@ const cloudinaryV2 = cloudinary.v2;
 const enviromentVariables = require('dotenv').config().parsed;
 
 cloudinaryV2.config({
-    cloud_name: enviromentVariables.CLOUDINARY_CLOUD_NAME,
-    api_key: enviromentVariables.CLOUDINARY_API_KEY,
-    api_secret: enviromentVariables.CLOUDINARY_API_SECRET
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 export default cloudinaryV2;
